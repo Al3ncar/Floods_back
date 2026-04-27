5f15acda4cf633d332697d495464437c88d62250
Al3ncar <alencar29htcss@gmail.com>
2026-04-27 16:01:50 -0300

    feat: upgrade new routes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,10 @@
-import express from "express"
-import { Router } from "express"
+import express from "express";
+import { Router } from "express";
+import useControl  from "../controllers/controllers.js";
 
-const router = Router()
+const router = Router();
 
-router.get('/get', (req, res) => {
-  res.send('Teste de api')
-})
- 
-export {router}
+router.get("/users", useControl.getAll);
+router.post("/users", useControl.addUsers);
+
+export default router
