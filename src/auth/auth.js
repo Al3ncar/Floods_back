3d56c86ffdf4c5aab4f9ae1fa273b1f312386900
Al3ncar <alencar29htcss@gmail.com>
2026-04-30 01:48:31 -0300

    feat: added auth functions and valid

---
+++ src/auth/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const auth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ msg: "Token Invalido" });
+  }
+
+  try {
+    const decode = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    req.user = decode;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token invalido" });
+  }
+};
+
+export default auth;
