8f29641c9da25d1badcc3294370e058902432016
Al3ncar <alencar29htcss@gmail.com>
2026-04-30 01:52:39 -0300

    feat: added validUser and Auth in routers

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,22 @@
 import express from "express";
 import { Router } from "express";
-import useControl  from "../controllers/controllers.js";
+import useControl from "../controllers/controllers.js";
+import validUser from "../validator/joiUser.js";
+import auth from "../auth/auth.js";
 
 const router = Router();
-
 router.get("/users", useControl.getAll);
-router.post("/users", useControl.createUser);
+router.post("/users", validUser, useControl.createUser);
 router.put("/users/:id", useControl.roleEditUser);
 
 router.get("/users/volunteer", useControl.getAllVolunteer);
 router.get("/users/help-me", useControl.getAllHelpMe);
 
 router.get("/requests", useControl.requestsAll);
-router.post("/requests", useControl.createRequest)
-router.put("/requests/:id", useControl.editRequest)
-router.delete("/requests/:id", useControl.removeRequest)
+router.post("/requests", auth, useControl.createRequest);
+router.put("/requests/:id", useControl.editRequest);
+router.delete("/requests/:id", useControl.removeRequest);
 
-router.post("/requests/:id/applications", useControl.removeRequest)
- 
-export default router
+router.post("/requests/:id/applications", useControl.removeRequest);
+
+export default router;
