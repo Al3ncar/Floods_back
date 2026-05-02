59cda2780b9a12d83d02162e899e64c269bfc5f8
Al3ncar <alencar29htcss@gmail.com>
2026-05-02 03:00:35 -0300

    feat: added new routes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,23 @@
 import express from "express";
 import { Router } from "express";
+
+import validRequest from "../validator/joiRequests.js"
 import useControl from "../controllers/controllers.js";
+import authorize from "../utils/authorizeRoles.js";
 import validUser from "../validator/joiUser.js";
 import auth from "../auth/auth.js";
 
 const router = Router();
 
 router.post("/login", useControl.login);
+
 router.get("/api/users", useControl.getAll);
 router.post("/api/users", validUser, useControl.createUser);
-router.put("/api/users/:id", auth, useControl.roleEditUser);
-
-router.get("/api/users/volunteer", useControl.getAllVolunteer);
-router.get("/api/users/help-me", useControl.getAllHelpMe);
+router.put("/api/users/:id", auth, useControl.editUserData);
+router.delete("/users/:id", auth, useControl.deleteUser);
 
 router.get("/api/requests", useControl.requestsAll);
-router.post("/api/requests", auth, useControl.createRequest);
+router.post("/api/requests", validRequest, auth, useControl.createRequest);
 router.put("/api/requests/:id", auth, useControl.editRequest);
 router.delete("/api/requests/:id", auth, useControl.removeRequest);
 
